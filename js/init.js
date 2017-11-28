87ca239908dd7134a7ecd8906400f7fab6d1253a
lisarko8077 <mirkocoppola80@gmail.com>
2017-11-28 11:05:45 +0200

    added media with lightbox

--- js/init.js
+++ js/init.js
@@ -4,5 +4,28 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+
+    /*// Get the modal
+    var modal = $("#myModal");
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = $(".wrap-img");
+    var modalImg = $("#img01");
+
+    $(".wrap-img img").on("click", function(){
+
+        modal.css("display", "block");
+        modalImg.attr("src", $(this).attr("src"));
+
+    });
+
+    // Get the <span> element that closes the modal
+    var span = $(".close");
+
+    // When the user clicks on <span> (x), close the modal
+    span.on("click", function() {
+      modal.css("display", "none");
+    });*/
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
